76f10c575a8acfef774fbdcd97959ffc2b12fb1d
Grace-Avor <graceavor2021@gmail.com>
2023-09-22 13:15:43 +0100

    Added drag and drop feature and made adjustments

--- src/Components/ImageCard/ImageCard.jsx
+++ src/Components/ImageCard/ImageCard.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { Draggable } from "react-beautiful-dnd";
 
 export const ImageCard = (props) => {
   return (
-    <div className="First-row">
-      <div className="images">
-        <img loading="lazy" src={props.url} alt={props.title} />
-        <p className="text-center fw-bold firstP">{props.title}</p>
-      </div>
-    </div>
+    <Draggable draggableId={props.id} index={props.index}>
+      {(provided) => (
+        <div
+          className="First-row"
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <div className="images">
+            <img src={props.url} alt={props.title} loading="lazy" />
+            <p className="text-center fw-bold firstP">{props.title}</p>
+          </div>
+        </div>
+      )}
+    </Draggable>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { DragDropContext } from "react-beautiful-dnd";
+
 import { data } from "../../data";
+import { StrictModeDroppable } from "../../helper";
 import { Header, ImageCard, SearchInput } from "../../Components";
 
 export const Home = () => {
   const [searchValue, setSearchValue] = useState("");
+  const [gallery, setGallery] = useState(data);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const filterGallery = data.filter(({ title }) =>
+  const filterGallery = gallery.filter(({ title }) =>
     title.toLowerCase().includes(searchValue)
   );
+
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const updatedGallery = [...gallery];
+    const [reorderedItem] = updatedGallery.splice(result.source.index, 1);
+    updatedGallery.splice(result.destination.index, 0, reorderedItem);
+    setGallery(updatedGallery);
+  };
+
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 2000);
+  }, []);
 
   return (
     <main className="MainContainer">
@@ -17,12 +35,30 @@ export const Home = () => {
           onChange={({ target }) => setSearchValue(target.value.toLowerCase())}
         />
       </div>
-
-      <div className="Container">
-        {filterGallery.map((pics) => (
-          <ImageCard {...pics} key={pics.id}/>
-        ))}
-      </div>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        {isLoading ? (
+          <p className="loading-text text-center">Loading...</p>
+        ) : (
+          <StrictModeDroppable droppableId="image-gallery">
+            {(provided) => (
+              <div
+                className="Container"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {filterGallery.map((pics, index) => (
+                  <ImageCard
+                    {...pics}
+                    key={pics.id}
+                    id={pics.id}
+                    index={index}
+                  />
+                ))}
+              </div>
+            )}
+          </StrictModeDroppable>
+        )}
+      </DragDropContext>
     </main>
   );
 };
